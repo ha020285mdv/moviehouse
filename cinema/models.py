515a2e4176d8b61332a2192a905cf10fa9b9a448
Dimon <ha020285mdv@gmail.com>
2022-08-04 13:05:19 +0300

    models and users login, register, logout added

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,108 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models import CASCADE
+from django.utils import timezone
 
-# Create your models here.
+AGE_CHOICES = (
+    (1, 'all'),
+    (2, '13+'),
+    (3, '18+')
+)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    genres = models.ManyToManyField(Genre)
+    description = models.TextField()
+    director = models.CharField(max_length=100)
+    starring = models.CharField(max_length=300)
+    trailer = models.CharField(max_length=300, blank=True, null=True)
+    teaser = models.CharField(max_length=300, blank=True, null=True)
+    img_landscape = models.ImageField(blank=True, null=True)
+    img_standard = models.ImageField(blank=True, null=True)
+    img_small = models.ImageField(blank=True, null=True)
+    age_policy = models.PositiveSmallIntegerField(choices=AGE_CHOICES, default=1)
+
+    def __str__(self):
+        return self.title
+
+
+class Hall(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    sits_rows = models.PositiveSmallIntegerField()
+    sits_cols = models.PositiveSmallIntegerField()
+
+    @property
+    def hall_capacity(self):
+        return self.sits_cols * self.sits_rows
+
+    def __str__(self):
+        return f'{self.name} {self.hall_capacity} sits'
+
+
+class MovieSession(models.Model):
+    settings = models.ForeignKey('MovieSessionSettings', on_delete=CASCADE)
+    date = models.DateField()
+    sits = models.JSONField()
+
+    def __str__(self):
+        return f'{self.settings.hall.name}: {self.date} ' \
+               f'{self.settings.time_start} - "{self.settings.movie.title[:20]}..."'
+
+
+class MovieSessionSettings(models.Model):
+    hall = models.ForeignKey(Hall, on_delete=CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=CASCADE)
+    date_start = models.DateField(default=timezone.now)
+    date_end = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    price = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f'{self.movie} ({self.hall.name}) {self.date_start} to ' \
+               f'{self.date_end} ({self.time_start}-{self.time_end}) {self.price}$'
+
+    # Может изменять зал или сеанс, если не было куплено ни одного билета в этот зал или на этот сеанс.
+    def save(self, **kwargs):
+        if self.id and True:    # check if empty
+            MovieSession.objects.filter(settings=self).delete()
+        super().save(**kwargs)
+        delta = self.date_end - self.date_start
+        for day in range(delta.days + 1):
+            MovieSession.objects.create(settings=self,
+                                        date=self.date_start + timezone.timedelta(days=day),
+                                        sits={sit_number: False for sit_number in range(1, self.hall.hall_capacity + 1)}
+                                        )
+
+
+class CinemaUser(AbstractUser):
+    email = models.EmailField(max_length=150, blank=False, unique=True)
+    first_name = models.CharField(max_length=150, blank=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        app_label = 'cinema'
+
+    def __str__(self):
+        return f'{self.first_name or self.email}'
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(CinemaUser, on_delete=CASCADE)
+    session = models.ForeignKey(MovieSession, on_delete=CASCADE)
+    datetime = models.DateTimeField(auto_now=True)
+    sits = models.JSONField()
+
+    def __str__(self):
+        return f'{self.customer} {len(self.sits)} sits in {self.datetime}'
--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LogoutView, LoginView
+from django.views.generic import TemplateView, CreateView
 
-# Create your views here.
+from cinema.forms import CustomUserCreationForm
+from cinema.models import CinemaUser
+
+
+class LoginView(LoginView):
+    success_url = '/'
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return self.success_url
+
+
+class RegisterView(CreateView):
+    model = CinemaUser
+    form_class = CustomUserCreationForm
+    success_url = '/'
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        to_return = super().form_valid(form)
+        login(self.request, self.object)
+        msg = 'You have been successfully registered and logged in!'
+        messages.success(self.request, msg)
+        return to_return
+
+
+class LogoutView(LoginRequiredMixin, LogoutView):
+    next_page = '/'
+    login_url = 'login/'
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
